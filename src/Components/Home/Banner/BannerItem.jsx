0adc3307e80066c7a270afced3f596bfe4e5c2ce
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2023-07-24 08:11:02 +0600

    post API set

--- src/Components/Home/Banner/BannerItem.jsx
+++ src/Components/Home/Banner/BannerItem.jsx
@@ -12,7 +12,7 @@ const BannerItem = ({ slide }) => {
       <div className=" absolute  transform -translate-y-1/2 left-24 top-1/4">
         <h1 className="text-6xl font-bold text-white">
           Affordable <br />
-          Price for ca <br />
+          Price for car <br />
           Servicing
         </h1>
       </div>
--- src/Components/Home/Services/Services.jsx
+++ src/Components/Home/Services/Services.jsx
@@ -4,7 +4,7 @@ import ServiceCard from "./ServiceCard";
 const Services = () => {
   const [services, setservices] = useState([]);
   useEffect(() => {
-    fetch("services.json")
+    fetch("http://localhost:5000/services")
       .then((res) => res.json())
       .then((data) => setservices(data));
   }, []);
